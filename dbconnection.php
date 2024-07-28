4a159dfe38621748de79b9c62671ff4fbbbf3010
Victor J Paredes <67197705+vparedes2@users.noreply.github.com>
2024-07-28 12:28:44 -0300

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,5 +1,13 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "crm-gestion");
-if(mysqli_connect_errno()){
-echo "Connection Fail".mysqli_connect_error();
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+
+try {
+    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    exit;
 }
